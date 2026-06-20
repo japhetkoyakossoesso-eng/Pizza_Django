3e547aecf0021a4848cb46336eab433a2f777abe
Japhet Koyakosso-Esso <japhetkoyakossoesso@gmail.com>
2026-06-20 13:00:19 +0200

    first commit Pizza_django

---
+++ pizza/menu/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+class Pizza(models.Model):
+    nom = models.CharField(max_length=400)
+    prix = models.FloatField(default=0)
+    vegetarienne = models.BooleanField(default=False)
