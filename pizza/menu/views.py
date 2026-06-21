3468bec35f9a0b25566039a18f1834d6730848d5
Japhet Koyakosso-Esso <japhetkoyakossoesso@gmail.com>
2026-06-21 15:33:08 +0200

    mise à jour

--- pizza/menu/views.py
+++ pizza/menu/views.py
@@ -9,5 +9,5 @@ def index(request):
     pizzas_names_and_prices = [pizza.nom + " : " + str(pizza.prix) + " € " for pizza in pizzas]
     pizzas_names_and_prices_str = ", ".join(pizzas_names_and_prices)
     return HttpResponse("Les pizzas : " + pizzas_names_and_prices_str)"""
-   pizzas = Pizza.objects.all()
+   pizzas = Pizza.objects.all().order_by('prix')
    return render(request, 'menu/index.html', {'pizzas' : pizzas})
