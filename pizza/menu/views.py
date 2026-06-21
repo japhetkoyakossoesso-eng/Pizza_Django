0680fa8bab4694c31adbce34f1b79b8bfc64cda0
Japhet Koyakosso-Esso <japhetkoyakossoesso@gmail.com>
2026-06-21 11:36:10 +0200

    création de la page html et mise à jour du code views.py

--- pizza/menu/views.py
+++ pizza/menu/views.py
@@ -5,7 +5,9 @@ from .models import Pizza
 
 # / menu
 def index(request):
-    pizzas = Pizza.objects.all()
-    pizzas_names = [pizza.nom for pizza in pizzas]
-    pizzas_names_str = ", ".join(pizzas_names)
-    return HttpResponse("Les pizzas : " + pizzas_names_str)
+   """ pizzas = Pizza.objects.all()
+    pizzas_names_and_prices = [pizza.nom + " : " + str(pizza.prix) + " € " for pizza in pizzas]
+    pizzas_names_and_prices_str = ", ".join(pizzas_names_and_prices)
+    return HttpResponse("Les pizzas : " + pizzas_names_and_prices_str)"""
+   pizzas = Pizza.objects.all()
+   return render(request, 'menu/index.html', {'pizzas' : pizzas})
